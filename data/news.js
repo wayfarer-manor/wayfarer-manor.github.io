d6099ab7709879ee464044f287a41e4a1b3e6b1e
Whisperito <pimentauintello@gmail.com>
2025-08-20 09:15:21 +0200

    Modifications qui seront validées : 	modifié :         data/css/styles.css 	nouveau fichier : data/icons/ysh.png 	modifié :         data/news.js

--- data/news.js
+++ data/news.js
@@ -6,7 +6,7 @@ var newsData = [
      "iconPath": "data/icons/4.png", 
      "verticalPadding": 30,
      "type": "Mods/Translations",
-     "News": 'Translations are finally 100% complete - aside from patches and bug fixes the mod is now complete - and will recieve no further <strong>major</strong> updates! It can be found <a href="https://gamebanana.com/mods/480884">here</a>.'
+     "News": 'Translations are finally 100% complete - aside from patches and bug fixes the mod is now complete - and will recieve no further <strong>major</strong> updates! It can be found <a class=hyperlink href="https://gamebanana.com/mods/480884">here</a>.'
     },
   {
      "Game": "Yo-kai Watch 1 for Switch",
@@ -16,4 +16,12 @@ var newsData = [
      "type": "Official",
      "News": 'Free Five-Star Coin reward at Lambert Post Office, trophy room now only shows trophies that have been obtained and awevil now appears like intended. '
     },
+  {
+     "Game": "Y School Heroes: Bustlin' School Life",
+     "Date": '2025-08-13',
+     "Title":'Y School Heroes French patch has now released.',
+     "iconPath": "data/icons/ysh.png",
+     "type":"Mods/Translations",
+     "News": 'While it remains a very early build. It still translates a few menus, a few names, and Chapter1 entirely! It can be found <a class=hyperlink href="https://gamebanana.com/mods/610368">here</a>.'
+     }
 ]
