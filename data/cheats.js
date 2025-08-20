4b316a110fa85805e9a6aa240d4919a3d7ff42ed
Whisperito <pimentauintello@gmail.com>
2025-08-20 08:38:56 +0200

    Cheats.js a été modifié

--- data/cheats.js
+++ data/cheats.js
@@ -1,75 +1,99 @@
 var cheatsData = [
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - remove item cooldown in battles TODO: ADD THIS LATER.", 
-     "Code": `D3000000 08000000
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - remove item cooldown in battles TODO: ADD THIS LATER.", 
+        "Code": `D3000000 08000000
 207BC2C9 00000000
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - remove the player's shadow.", 
-     "Code": `D3000000 08000000
-007C7638 00000000
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - remove the player's shadow.", 
+        "Code": `D3000000 08000000
+207C7638 00000000
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Remove Gargaros/Snartle fights from crosswalk events.", 
-     "Code": `D3000000 08000000
-006D0C3C 00000000
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Remove Gargaros/Snartle fights from crosswalk events.", 
+        "Code": `D3000000 08000000
+206D0C3C 00000000
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Instant Ogralos fight next crosswalk event.", 
-     "Code": `D3000000 08000000
-006D0C3B 000000FE
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "NA", 
+        "Title": "CTRPF - Remove Gargaros/Snartle fights from crosswalk events.", 
+        "Code": `D3000000 08000000
+206D0C3C 00000000
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Force Watch Rank S.", 
-     "Code": `D3000000 08000000
-006D123A 00000005
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Instant Ogralos fight next crosswalk event.", 
+        "Code": `D3000000 08000000
+206D0C3B 000000FE
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Oneshot enemies.", 
-     "Code": `D3000000 08000000
-007B8794 00000001
-007B82AC 00000001
-007B8C7C 00000001
-007B9164 00000001
-007B964C 00000001
-007B9B34 00000001
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "NA", 
+        "Title": "CTRPF - Instant Ogralos fight next crosswalk event.", 
+        "Code": `D3000000 08000000
+206D0C3B 000000FE
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Ally Yo-kai Immortality.", 
-     "Code": `D3000000 08000000
-007B653C 00007FFF
-007B6A24 00007FFF
-007B6F0C 00007FFF
-007B73FC 00007FFF
-007B78DC 00007FFF
-007B7DC4 00007FFF
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Force Watch Rank S.", 
+        "Code": `D3000000 08000000
+206D123A 00000005
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Save/Load Y-Coordinate (Dpad-left to save, Dpad-right to restore).", 
-     "Code": `DFFFFFFE 00000001
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "NA", 
+        "Title": "CTRPF - Force Watch Rank S.", 
+        "Code": `D3000000 08000000
+206D123A 00000005
+D2000000 00000000`
+    },
+    {
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Oneshot enemies.", 
+        "Code": `D3000000 08000000
+107B8794 00000001
+107B82AC 00000001
+107B8C7C 00000001
+107B9164 00000001
+107B964C 00000001
+107B9B34 00000001
+D2000000 00000000`
+    },
+    {
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Ally Yo-kai Immortality.", 
+        "Code": `D3000000 08000000
+107B653C 00007FFF
+107B6A24 00007FFF
+107B6F0C 00007FFF
+107B73FC 00007FFF
+107B78DC 00007FFF
+107B7DC4 00007FFF
+D2000000 00000000`
+    },
+    {
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Save/Load Y-Coordinate (Dpad-left to save, Dpad-right to restore).", 
+        "Code": `DFFFFFFE 00000001
 DD000000 00000020
 D9000000 08810114
 D6000000 01E81000
@@ -81,57 +105,95 @@ D6000000 08710114
 D0000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Max Oni Orbs Terror Time - collect one orb to activate.", 
-     "Code": `D3000000 08000000
-009E3D24 00007FFE
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Max Oni Orbs Terror Time - collect one orb to activate.", 
+        "Code": `D3000000 08000000
+109EBEB0 00007E00
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Infinite Crank-a-kai Uses (Appears as 30/30).", 
-     "Code": `D3000000 08000000
-006D0C04 0000001E
-006D0C0C 0000001E
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Infinite Crank-a-kai Uses (Appears as 30/30).", 
+        "Code": `D3000000 08000000
+206D0C04 0000001E
+206D0C0C 0000001E
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Infinite Money.", 
-     "Code": `D3000000 08000000
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "NA", 
+        "Title": "CTRPF - Infinite Crank-a-kai Uses (Appears as 30/30).", 
+        "Code": `D3000000 08000000
+206D0C04 0000001E
+206D0C0C 0000001E
+D2000000 00000000`
+    },
+    {
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Infinite Money.", 
+        "Code": `D3000000 08000000
 006CCBE0 7FFFFFFF
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Infinite JP.", 
-     "Code": `D3000000 08000000
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "NA", 
+        "Title": "CTRPF - Infinite Money.", 
+        "Code": `D3000000 08000000
+006CCBE0 7FFFFFFF
+D2000000 00000000`
+    },
+    {
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Infinite JP.", 
+        "Code": `D3000000 08000000
 006CCBE4 7FFFFFFF
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Infinite GP.", 
-     "Code": `D3000000 08000000
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "NA", 
+        "Title": "CTRPF - Infinite JP.", 
+        "Code": `D3000000 08000000
+006CCBE4 7FFFFFFF
+D2000000 00000000`
+    },
+    {
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Infinite GP.", 
+        "Code": `D3000000 08000000
 006CCBE8 7FFFFFFF
 D2000000 00000000`
     },
     {
-     "Game": "Yo-kai Watch 2: Psychic Specters", 
-     "Region": "EUR", 
-     "Title": "CTRPF - Shrink Player (0.5x size) - Does not affect hitbox.", 
-     "Code": `D3000000 08000000
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "NA", 
+        "Title": "CTRPF - Infinite GP.", 
+        "Code": `D3000000 08000000
+006CCBE8 7FFFFFFF
+D2000000 00000000`
+    },
+    {
+        "Game": "Yo-kai Watch 2: Psychic Specters", 
+        "Region": "EUR", 
+        "Title": "CTRPF - Shrink Player (0.5x size) - Does not affect hitbox.", 
+        "Code": `D3000000 08000000
 007C760C 3F000000
 007C7610 3F000000
 007C7614 3F000000
 D2000000 00000000`
-    },    
-]
-
-
-
+    },
+    {
+        "Game": "Yo-kai Watch 2: Psychic Specters",
+        "Region": "NA",
+        "Title": "CTRPF - Sneaky Steps, makes you invisible when still (visual only!!)",
+        "Code": `D3000000 08000000
+10BF22D1 00005678
+D2000000 00000000`
+    }
+];
